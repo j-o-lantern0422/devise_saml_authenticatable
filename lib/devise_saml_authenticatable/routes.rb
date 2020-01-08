e36d593317959793bd7cea87d7b8f425c4efa58d
nishihata <nishihata@pepabo.com>
2020-01-08 14:51:54 +0900

    patch short-term fix

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -3,7 +3,7 @@ ActionDispatch::Routing::Mapper.class_eval do
   def devise_saml_authenticatable(mapping, controllers)
     if ::Devise.saml_route_helper_prefix
       prefix = ::Devise.saml_route_helper_prefix
-      resource :session, only: [], controller: controllers[:saml_sessions], path: '' do
+      resource :session, only: [], controller: 'devise/saml_sessions', path: '' do
         get :new, path: 'saml/sign_in', as: "new_#{prefix}"
         post :create, path: 'saml/auth', as: prefix
         match :destroy, path: mapping.path_names[:sign_out], as: "destroy_#{prefix}", via: mapping.sign_out_via
@@ -11,7 +11,7 @@ ActionDispatch::Routing::Mapper.class_eval do
         match :idp_sign_out, path: 'saml/idp_sign_out', as: "idp_destroy_#{prefix}", via: [:get, :post]
       end
     else
-      resource :session, only: [], controller: controllers[:saml_sessions], path: '' do
+      resource :session, only: [], controller: 'devise/saml_sessions', path: '' do
         get :new, path: 'saml/sign_in', as: 'new'
         post :create, path: 'saml/auth'
         match :destroy, path: mapping.path_names[:sign_out], as: 'destroy', via: mapping.sign_out_via
