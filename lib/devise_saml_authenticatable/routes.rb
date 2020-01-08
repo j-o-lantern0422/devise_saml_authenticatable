15a1ea17953588a6dba7e209a734ee9d18407e2d
nishihata <nishihata@pepabo.com>
2020-01-08 15:29:53 +0900

    fix path

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -11,7 +11,7 @@ ActionDispatch::Routing::Mapper.class_eval do
         match :idp_sign_out, path: 'saml/idp_sign_out', as: "idp_destroy_#{prefix}", via: [:get, :post]
       end
     else
-      resource :session, only: [], controller: 'devise/saml_sessions', path: '' do
+      resource :session, only: [], controller: controllers[:saml_sessions], path: '' do
         get :new, path: 'saml/sign_in', as: 'new'
         post :create, path: 'saml/auth'
         match :destroy, path: mapping.path_names[:sign_out], as: 'destroy', via: mapping.sign_out_via
